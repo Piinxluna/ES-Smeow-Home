13c02abe0de8507a77863d10e65e00f2b18fd39e
Wuaimi <10327sornpicha@gmail.com>
2024-11-24 16:00:10 +0700

    update config

--- smeow-home/src/components/basic/Button.tsx
+++ smeow-home/src/components/basic/Button.tsx
@@ -1,50 +1,54 @@
-'use client'
+'use client';
 
-// import react
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Button({
   variant = 'primary',
   children,
-  className,
+  className = 'text-black bg-white hover:bg-lightgray/80',
   onClick,
   href,
   target,
 }: {
-  variant?: 'primary' | 'secondary' | 'outline' | 'text' | 'disabled'
-  children: React.ReactNode
-  className?: string
-  onClick?: Function
-  href?: string
-  target?: string
+  variant?: 'primary' | 'secondary' | 'outline' | 'pink';
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  href?: string;
+  target?: string;
 }) {
-  let theme = 'text-black bg-white hover:lightgray/80'
-  if (variant === 'secondary')
-    theme = 'text-black bg-lightgray hover:bg-darkgray'
-  if (variant === 'outline')
+  let theme = 'text-black font-semibold bg-white hover:bg-gray-500';
+  if (variant === 'secondary') {
+    theme = 'text-black font-semibold bg-white hover:bg-lightgray1';
+  }
+  if (variant === 'outline') {
     theme =
-      'text-black border-2 border-darkgray bg-white hover:text-white hover:bg-white shadow-none'
+      'text-black font-semibold border-2 border-gray-950 bg-white hover:text-white hover:bg-black shadow-none';
+  }
+  if (variant === 'pink') {
+    theme = 'text-white font-semibold bg-epink hover:bg-edarkpink';
+  }
 
-  // return
   if (href) {
     return (
-      <Link href={href} legacyBehavior>
+      <Link href={href}>
         <a
           target={target}
-          className={`px-4 py-2 rounded-md shadow ${theme} ${className}`}>
+          className={`px-4 py-2 rounded-md shadow ${theme} ${className}`}
+        >
           {children}
         </a>
       </Link>
-    )
-  } else {
-    return (
-      <button
-        onClick={() => {
-          onClick ? onClick() : ''
-        }}
-        className={`px-4 py-2 rounded-md shadow ${theme} ${className}`}>
-        {children}
-      </button>
-    )
+    );
   }
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`px-4 py-2 rounded-md shadow ${theme} ${className}`}
+    >
+      {children}
+    </button>
+  );
 }
