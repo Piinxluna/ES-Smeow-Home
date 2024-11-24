c0055cecab02b8f1c23639f0d194fd85733974fb
Piinxluna <pingpingkuss@gmail.com>
2024-11-24 16:02:41 +0700

    test color

--- smeow-home/src/app/page.tsx
+++ smeow-home/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Button variant='outline'>click me</Button>
-      <p className='text-xl font-bold text-blue'>Test</p>
+      <p className='text-xl font-bold text-blue-500'>Test</p>
+      <p className='text-xl font-bold text-ered-100'>Test</p>
+      <p className='text-xl font-bold text-lightgray1'>Test</p>
     </main>
   )
 }
