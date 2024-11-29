4205d5300a3ea6ef518e061cc27d842fa8afa3e1
Wuaimi <10327sornpicha@gmail.com>
2024-11-29 12:20:48 +0700

    home page fix

--- smeow-home/src/app/page.tsx
+++ smeow-home/src/app/page.tsx
@@ -1,51 +1,34 @@
-import Button from '@/components/basic/Button'
-import BacktoHomeButton from '@/components/basic/BackToHomeButton'
+import Image from 'next/image'
 import NavBar from '@/components/navbar/NavBar'
 import CurrentTemp from '@/components/basic/CurrentTemp'
 import LastFed from '@/components/food/LastFed'
 import Header from '@/components/basic/Header'
 import Remaining from '@/components/basic/Remaining'
 
-const VideoContainer = () => {
-  return (
-    <div style={{
-      position: 'relative',
-      
-      backgroundImage: "url('/resources/web/LiveVideoBackground1.png')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      minHeight: '500px',
-      width: '100%'
-    }}>
-      <div style={{
-        position: 'absolute',
-        top: '20px',
-        left: '20px',
-        right: '20px',
-        bottom: '20px',
-        backgroundColor: '#463F3A', // Gray background for video area
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-        {/* Place your video component here */}
-      </div>
-    </div>
-  );
-};
 
 export default function Home() {
   return (
-    <main className='flex flex-row justify-items-center px-16 py-12 max-h-full'>
-      <div className='flex flex-col justify-items-center items-center w-[75%]'>
-        <Header className='mb-8'></Header>
-        <NavBar variant='secondary'/>
-        <div className="flex flex-col items-center w-full max-w-[800px] bg-gray-200 rounded-lg shadow-lg mt-6">
-        <VideoContainer />
+    <main className='flex flex-row justify-items-center px-12 py-8 max-h-full'>
+      <div className='flex flex-col ml-12 w-[70%]'>
+        <div className="flex flex-col items-center w-full max-w-[800px] rounded-lg mt-6">
+        <NavBar variant='secondary' className='absolute z-10 flex justify-items-center items-center'/>
+        <div className='relative w-full'>
+          <Image
+            src='/resources/web/LiveVideoBackground1.png'
+            width={800}
+            height={700}
+            alt="Meow Background"
+            className='relative'
+            >
+          </Image>
+          <div className='absolute top-0 mt-40 ml-10 w-[90%] h-[76%] bg-darkgray rounded-lg z-10 flex items-center justify-center'>
+          </div>
+        </div>
       </div>
       </div>
-      <div className='flex flex-col justify-items-center items-center space-y-4 mt-20 w-[25%]'>
-        <CurrentTemp variant='primary' Temp={35} className='w-full content-center-'></CurrentTemp>
+      <div className='flex flex-col mr-12 justify-items-center items-center space-y-4 w-[30%]'>
+        <Header className='mt-12 mb-6'></Header>
+        <CurrentTemp variant='primary' Temp={35} className='w-full'></CurrentTemp>
         <Remaining variant='water' percent={87} nextRefill='1 Day 14 Hours'  className='w-full'></Remaining>
         <LastFed percent={40} nextRefill={'1 Day 14 Hours'} lastFed={'18 Nov, 15:26'} petStatus={'Not eaten'} className='w-full'></LastFed>
       </div>
--- smeow-home/src/components/navbar/NavBar.tsx
+++ smeow-home/src/components/navbar/NavBar.tsx
@@ -10,7 +10,7 @@ export default function NavBar({
     if(variant === 'primary'){
     return (
         <div className="rounded-lg w-90 h-fit bg-epink md:p-4 py-2 px-6">
-            <h1 className="text-white font-bold text-center md:text-2xl text-xl mb-2 mt-1">Control</h1>
+            <h1 className={`text-white font-bold text-center md:text-2xl text-xl mb-2 mt-1 ${className}`}>Control</h1>
             <div className="flex space-x-2 mb-2" >
                 <Button variant='primary' href='/food/auto'>Food</Button>
                 <Button variant='primary' href='/water'>Water</Button>
@@ -21,12 +21,12 @@ export default function NavBar({
     );}
     else if (variant === 'secondary'){
         return (
-            <div className="rounded-lg h-fit bg-epink md:p-4 py-2 px-6 flex space-x-2 mb-2 items-center">
+            <div className={`rounded-lg h-fit bg-epink md:px-2 md:py-4 py-2 px-6 flex space-x-2 mb-2 items-center ${className}`}>
                 <h1 className="text-white font-bold text-center md:text-xl text-l mr-2">Control</h1>
-                <Button variant='secondary' href='/food/auto' >Food</Button>
-                <Button variant='secondary' href='/water'>Water</Button>
-                <Button variant='secondary' href='/play'>Laser</Button>
-                <Button variant='secondary' href='/watch-live'>WatchLive</Button>
+                <Button variant='secondary' href='/food/auto' className="p-2 bg-white" >Food</Button>
+                <Button variant='secondary' href='/water' className="p-2 bg-white">Water</Button>
+                <Button variant='secondary' href='/play' className="p-2 bg-white">Laser</Button>
+                <Button variant='secondary' href='/watch-live' className="p-2 bg-white">WatchLive</Button>
             </div>
         );
     }
